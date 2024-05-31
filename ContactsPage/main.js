01b7a020f2e0cc675cea33a62032e49ff3d8bf1b
Max Bugakov <max.bugakov18@gmail.com>
2024-05-31 14:54:39 +0300

    com

---
+++ ContactsPage/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // Выпадающее меню.
+    let click_menu_flag=false;
+    $(".header_burger-menu").on("click", function() {
+        if (click_menu_flag==false) {
+            $('.pop-up-menu').addClass('open-menu');
+            click_menu_flag=true;
+            $('html').css('overflow-y', 'hidden');
+            $('.header_burger-menu').css('background-image', 'url(../static/close_ico.png)');
+            $('.header_burger-menu').css('background-size', '120%');
+        }
+        else {
+            $('.pop-up-menu').removeClass('open-menu');
+            click_menu_flag=false;
+            $('html').css('overflow-y', 'scroll');
+            $('.header_burger-menu').css('background-image', 'url(../static/burger_menu_icon.png)');
+            $('.header_burger-menu').css('background-size', '100%');
+        }
+    });
+});
+
+
